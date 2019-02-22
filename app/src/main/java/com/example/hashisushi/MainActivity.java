c9f72528a1eaa53fca647c2f78ba3ccdd9bb4142
@correiarangel <correiarangel@bol.com.br>
2019-02-22 17:40:07 -0300

    Login screen

--- app/src/main/java/com/example/hashisushi/MainActivity.java
+++ app/src/main/java/com/example/hashisushi/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
     }
 }
