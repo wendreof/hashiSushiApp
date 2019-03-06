36e70ee9a372c1583bc482de60f1676ce7ace69b
wlf <wendreoofernandes@gmail.com>
2019-03-06 13:11:42 -0300

    Addin ActPoints

---
+++ app/src/main/java/com/example/hashisushi/views/ActPoints.java
@@ -0,0 +1,15 @@
+package com.example.hashisushi.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hashisushi.R;
+
+public class ActPoints extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_points);
+    }
+}
