c1a060815a72e70dfe5c6f7fc69f6752c211261b
Wendreo <wendreolf@gmail.com>
2019-05-24 22:59:54 -0300

    Saving the user e-mail with SharedPreferences

--- app/src/main/java/com/example/hashisushi/views/ActLogin.java
+++ app/src/main/java/com/example/hashisushi/views/ActLogin.java
@@ -13,10 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hashisushi.R;
+import com.example.hashisushi.utils.data.SecurityPreferences;
 import com.example.hashisushi.views.navigation.Navigation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,8 +31,7 @@ import java.util.Date;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class ActLogin extends AppCompatActivity implements View.OnClickListener
-{
+public class ActLogin extends AppCompatActivity implements View.OnClickListener {
     public static String STATUS = null;
     private Button btnEntrar;
     private Button btnCadastrar;
@@ -43,183 +44,158 @@ public class ActLogin extends AppCompatActivity implements View.OnClickListener
     private char controlBtn;
     private ConstraintLayout ActLogin;
     private FirebaseAuth userAuth;
+    private SecurityPreferences shared;
+    private Switch chkBxRememberPasswd;
+    private String emailUser;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.act_login);
+
+        this.shared = new SecurityPreferences(this);
         getSupportActionBar().hide();
 
-        //Travæ rotaçãø da tela
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);    //Trava a rotaçãø da tela
 
-        btnEntrar = findViewById(R.id.btnEntrar);
-        btnCadastrar = findViewById(R.id.btnCadastrar);
-        txtLogo = findViewById(R.id.txtLogoC);
-        edtEmail = findViewById(R.id.edtEmail);
-        edtSenha = findViewById(R.id.edtSenha);
-        ActLogin = findViewById(R.id.ActLogin);
-        //Chama metudo que altera fonte logo
-        fontLogo();
+        findViewByIds();
+
+        fontLogo();         //Chama metudo que altera fonte logo
 
         this.userAuth = FirebaseAuth.getInstance();
         //userAuth.signOut();
-
-        btnCadastrar.setOnClickListener(this);
-        btnEntrar.setOnClickListener(this);
         getDate();
+
+        emailUser = this.shared.getStoredString("EmailUserSaved");
+
+        setEmailUser();
     }
 
-    /*
-     * Para que a nova fonte seja exibida na tela,
-     * precisamos chamar um método na Activity que sobrescreva
-     * o contexto base com um Wrapper da biblioteca.
-     */
     @Override
-    protected void attachBaseContext(Context newBase)
-    {
+    protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
-    //Altera fonte do txtLogo
-    private void fontLogo()
-    {
+    private void fontLogo() {  //Altera fonte do txtLogo
         Typeface font = Typeface.createFromAsset(getAssets(), "RagingRedLotusBB.ttf");
         txtLogo.setTypeface(font);
     }
 
     @Override
-    public void onClick(View v)
-    {
-        if (v.getId() == R.id.btnEntrar)
-        {
+    protected void onResume() {
+        super.onResume();
+        this.setEmailUser();  //Carrega o e-mail de SharedPreferences quando existente!
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnEntrar) {
             controlBtn = 'E';
             startVibrate(90);
             validateFields();
 
-        }
-        else if (v.getId() == R.id.btnCadastrar)
-        {
+        } else if (v.getId() == R.id.btnCadastrar) {
             controlBtn = 'C';
             startVibrate(90);
             //validateFields();
             Intent it = new Intent(this, ActSignup.class);
             startActivity(it);
+        } else if (v.getId() == R.id.chkBxRememberPasswd) {
+            if (chkBxRememberPasswd.isChecked()) {
+                this.shared.storeString("EmailUserSaved", edtEmail.getText().toString());
+                //msgShort(emailUser);
+                ShowMSG("Seu e-mail será lembrado!");
+            } else {
+                this.shared.storeString("EmailUserSaved", "");
+                ShowMSG("Seu e-mail não será lembrado!");
+            }
         }
     }
 
     //login user in firebase
-    public void login(String email, String senha)
-    {
+    public void login(String email, String senha) {
         userAuth.signInWithEmailAndPassword(email, senha)
-                .addOnCompleteListener(ActLogin.this, new OnCompleteListener<AuthResult>()
-                {
+                .addOnCompleteListener(ActLogin.this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task)
-                    {
-                        if (task.isSuccessful())
-                        {
-                            msgShort("Bem vindo! ;)");
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            msgShort("Bem vindo! ;)" + emailUser);
                             initPromotion();
-                        }
-                        else
-                        {
+                        } else {
                             msgShort("Falha no acessar o app! :(");
                         }
                     }
                 });
     }
 
-    public void testUserAuth()
-    {
-        if (userAuth.getCurrentUser() != null)
-        {
+    public void testUserAuth() {
+        if (userAuth.getCurrentUser() != null) {
             msgShort("Usuário Logado!");
 
-        }
-        else
-        {
+        } else {
             msgShort("Usuário não esta Logado !");
         }
     }
 
-    private void initPromotion()
-    {
+    private void initPromotion() {
         //Intent it = new Intent(this, ActPromotion.class);
         Intent it = new Intent(this, Navigation.class);
         startActivity(it);
     }
 
     //Metudo que ativa vibração
-    public void startVibrate(long time)
-    {
+    public void startVibrate(long time) {
         // cria um obj atvib que recebe seu valor de context
         Vibrator atvib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         atvib.vibrate(time);
     }
 
-    public void validateFields()
-    {
+    public void validateFields() {
         //Declara variavel Recupra valores
         // Usando oque foi definido e referenciado
         email = edtEmail.getText().toString();
         senha = edtSenha.getText().toString();
-        if (cont <= 3)
-        {
+        if (cont <= 3) {
             //desisão para tratar campos em brando Se campos em branco
 
-            if (email.trim().isEmpty() || senha.trim().isEmpty())
-            {
+            if (email.trim().isEmpty() || senha.trim().isEmpty()) {
                 cont++;
-              //  msgShort("Por favor, digite seu e-mail e senha para entrar ou cadastre-se");
+                //  msgShort("Por favor, digite seu e-mail e senha para entrar ou cadastre-se");
                 ShowMSG("Por favor, digite e-mail e senha para entrar ou cadastre-se");
-            }
-            else
-            {
+            } else {
                 // mensagem(user+" logado !");
                 //msgShort("Seja Bem Vindo !");
 
-                if (controlBtn == 'E')
-                {
+                if (controlBtn == 'E') {
                     login(email, senha);
-                }
-                else if (controlBtn == 'C')
-                {
-                   // addUserLogin(email, senha);
+                } else if (controlBtn == 'C') {
+                    // addUserLogin(email, senha);
                 }
                 System.setProperty("STATUS_ENV", STATUS);
                 clearFields();
                 cont = 0;
             }//if campos
-        }
-        else
-        {
+        } else {
             finaliza();
         }//if cont
     }
 
-    private void finaliza()
-    {
+    private void finaliza() {
         msgShort("Hashi Sushi Finalizado !");
         // System.clearProperty("codigoUser");
         finish();
     }
 
-    private void msgShort(String msg)
-    {
+    private void msgShort(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void clearFields()
-    {
+    private void clearFields() {
         edtEmail.setText("");
         edtSenha.setText("");
     }
 
-    private void getDate()
-    {
+    private void getDate() {
         SimpleDateFormat dateFormat_hora = new SimpleDateFormat("HHmm");
 
         Calendar cal = Calendar.getInstance();
@@ -228,18 +204,38 @@ public class ActLogin extends AppCompatActivity implements View.OnClickListener
         String hora_atual = dateFormat_hora.format(data_atual);
         Integer intHora = Integer.parseInt(hora_atual);
 
-        if (intHora > 900 && intHora < 2200)
-        {
+        if (intHora > 900 && intHora < 2200) {
             STATUS = "Estamos atendendo!";
-        }
-        else
-        {
+        } else {
             STATUS = "Não estamos atendendo agora!";
         }
     }
 
-    private void ShowMSG(String msg)
-    {
+    private void ShowMSG(String msg) {
         Snackbar.make(ActLogin, msg, Snackbar.LENGTH_LONG).show();
+    }
+
+    private void setEmailUser() {
+        //if (!emailUser.equals("ValorNãoRecuperado") && chkBxRememberPasswd.isChecked()) {
+        if (!emailUser.equals("")) {
+            edtEmail.setText(emailUser);
+            chkBxRememberPasswd.setChecked(true);
+        } else {
+            edtEmail.setText(emailUser);
+        }
+    }
+
+    private void findViewByIds() {
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        txtLogo = findViewById(R.id.txtLogoC);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        ActLogin = findViewById(R.id.ActLogin);
+        chkBxRememberPasswd = findViewById(R.id.chkBxRememberPasswd);
+
+        btnCadastrar.setOnClickListener(this);
+        btnEntrar.setOnClickListener(this);
+        chkBxRememberPasswd.setOnClickListener(this);
     }
 }
