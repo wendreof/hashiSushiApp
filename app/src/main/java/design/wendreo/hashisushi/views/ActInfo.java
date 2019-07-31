cfbb21a172bb3c0e3fa437beb5743370909a5d66
@correiarangel <correiarangel@bol.com.br>
2019-07-31 11:27:56 -0300

    ActInfo

---
+++ app/src/main/java/design/wendreo/hashisushi/views/ActInfo.java
@@ -0,0 +1,15 @@
+package design.wendreo.hashisushi.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import design.wendreo.hashisushi.R;
+
+public class ActInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_info);
+    }
+}
